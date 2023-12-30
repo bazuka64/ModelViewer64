1672e93a6c14be31948892d1c32fce3df632bece
pentan0319 <pentan0319@gmail.com>
2023-12-30 10:29:38 +0900

    first

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+#include "config.h"
+
 int main()
 {
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "", NULL, NULL);
 
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 }
